7cd1a3f184569c6817dad6b969814cb43830ff03
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-09-21 00:36:21 +0900

    feat: integration GraphQL Module and build first graphql query api

--- apps/abutech-api/src/app.module.ts
+++ apps/abutech-api/src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot()], //-> abutech api server module da environmental variabllarni ishga tushurib beradi
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(), //-> abutech api server module da environmental variabllarni ishga tushurib beradi
+		GraphQLModule.forRoot({
+			driver: ApolloDriver, // -> graphql playground ni ishga tushiramiz quyidagi codelar bn
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService, AppResolver],
 })
 export class AppModule {}
